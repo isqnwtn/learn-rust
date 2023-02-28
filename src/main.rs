64ba506f8e8a72c4e0b2e1d81157c91b487f7855
Vismay Raj <vismayraj1@gmail.com>
2023-02-28 16:21:43 +0530

    Added a basic argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+/*
+ * Author : Vismay
+ * description : README.md
+ */
+use clap::Parser;
+use clap::ArgEnum;
+
+// A collection of resources while learning rust
+#[derive(Default,Parser,Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Arguments {
+    // package name
+    #[clap(arg_enum,short,long)]
+    example_type: ExampleKind,
+
+    // max depth
+    #[clap(short, long, default_value_t=true)]
+    interactive: bool, // depth
+}
+
+#[derive(ArgEnum, Clone, Debug, Default)]
+enum ExampleKind {
+    // basic examples in rust
+    #[default]
+    BasicExamples,
+
+    // functions in rust
+    Functions,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+    println!("{:?}", args);
 }
